4434c6793b2f6474ee2bc337d84786420b18e607
KathyLBonds <kathy.bonds@comcast.net>
2022-10-18 15:14:19 -0500

    Finished Project

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Operators_Exercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Exercise 1 and 2
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+
+            //Exercise 2
+
+            Console.WriteLine("Please enter the radius of your circle:");
+
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+        }
+    }
+}
+
+        
+
+
+       
+        
+
+        
+            
+
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        
+   
+
